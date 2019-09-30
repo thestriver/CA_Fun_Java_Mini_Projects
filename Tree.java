1dec26eb5b585ae9847a46d3515ff81684ba0433
thestriver <wannabeamumin@gmail.com>
2019-09-30 23:30:27 +0100

    Fun Codecademy java mini-projects

---
+++ Tree.java
@@ -0,0 +1,12 @@
+class Tree {
+    public static void main(String[] args) {
+        //plant a tree
+        System.out.println("I'm an infovore");
+        System.out.println("Ready to get my hands dirty!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
---
+++ madLibs.java
@@ -0,0 +1,33 @@
+class Tree {
+    public static void main(String[] args) {
+        public class MadLibs {
+            /*
+            This program generates a mad libbed story.
+            Author: Infovore
+            Date: 2/19/2049
+            */
+            public static void main(String[] args){
+                String name1 = "Tade";
+                String adjective1 = "good";
+                String adjective2 = "great";
+                String adjective3 = "motivated";
+                String verb1 = "sing";
+                String noun1 = "student";
+                String noun2 = "books";
+                String noun3 = "song";
+                String noun4 = "attendant";
+                String noun5 = "pies";
+                String noun6 = "babies";
+                String name2 = "Mother";
+                String place1 = "garden";
+                int number = 2098;
+
+                //The template for the story
+                String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+                System.out.println(story);
+            }
+        }
+
+    }
+}
