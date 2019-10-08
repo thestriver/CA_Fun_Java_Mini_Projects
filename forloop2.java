51bfc60e6350dc60fb9d9c6cdea5d81a121445ee
thestriver <wannabeamumin@gmail.com>
2019-10-08 21:52:01 +0100

    small loop

---
+++ forloop2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3loopsandarrays;
+
+/**
+ *
+ * @author 19038521
+ */
+public class forloop2 {
+     public static void main(String[] args) {
+        
+        // TODO code application logic here
+        for(int i =0 ; i<= 9; i++) {
+            System.out.println(i);
+            System.out.println(" squared ");
+            System.out.println(i*i);
+            
+            
+        
+        
+        }
+        
+        System.out.println("Goodbye");
+    
+    
+    
+    } 
+    
+}
