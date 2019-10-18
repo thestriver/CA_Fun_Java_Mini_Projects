a3e11f1e45e6986c1981cdb29aeff97088eddd52
thestriver <wannabeamumin@gmail.com>
2019-10-18 23:03:35 +0100

    minor edits

--- shipping.java
+++ shipping.java
@@ -1,32 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="1671.6">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica}
-    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; min-height: 14.0px}
-    span.Apple-tab-span {white-space:pre}
-  </style>
-</head>
-<body>
-<p class="p1">public class Order {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>boolean isFilled;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>double billAmount;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>String shipping;</p>
-<p class="p2"><span class="Apple-converted-space">  </span></p>
-<p class="p1"><span class="Apple-converted-space">  </span>public Order(boolean filled, double cost, String shippingMethod) {</p>
-<p class="p1"><span class="Apple-tab-span">	</span><span class="Apple-tab-span">	</span>if (cost &gt; 24.00) {</p>
-<p class="p1"><span class="Apple-converted-space">      </span>System.out.println("High value item!");</p>
-<p class="p1"><span class="Apple-converted-space">    </span>}</p>
-<p class="p1"><span class="Apple-converted-space">    </span>isFilled = filled;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>billAmount = cost;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>shipping = shippingMethod;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>}</p>
-<p class="p2"><span class="Apple-converted-space">  </span></p>
+public class Order {
+boolean isFilled;
+double billAmount;
+String shipping;
+public Order(boolean filled, double cost, String shippingMethod) {
+if (cost == 24.00) {
+  System.out.println("High value item!");
+
+
+}
+isFilled = filled;
+billAmount = cost;
+shipping = shippingMethod;
+
+}
+
 <p class="p1"><span class="Apple-converted-space">  </span>public void ship() {</p>
 <p class="p1"><span class="Apple-converted-space">    </span>if (isFilled) {</p>
 <p class="p1"><span class="Apple-converted-space">      </span>System.out.println("Shipping");</p>
