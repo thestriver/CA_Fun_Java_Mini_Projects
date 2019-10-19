48575463500ce24b2af2c38b164aa1b7c4880b0f
thestriver <wannabeamumin@gmail.com>
2019-10-19 22:42:29 +0100

    edits

--- RestaurantBooking.java
+++ RestaurantBooking.java
@@ -1,64 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="1671.6">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 14.0px; font: 12.0px Helvetica; color: #000000; -webkit-text-stroke: #000000}
-    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 14.0px; font: 12.0px Helvetica; color: #000000; -webkit-text-stroke: #000000; min-height: 12.0px}
-    span.s1 {font-kerning: none}
-    span.Apple-tab-span {white-space:pre}
-  </style>
-</head>
-<body>
-<p class="p1"><span class="s1">public class Reservation {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>int guestCount;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>int restaurantCapacity;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>boolean isRestaurantOpen;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>boolean isConfirmed;</span></p>
-<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span></span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>public Reservation(int count, int capacity, boolean open) {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>if (count &lt; 1 || count &gt; 8) {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>System.out.println("Invalid reservation!");</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>}</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>guestCount = count;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-tab-span">	</span><span class="Apple-tab-span">	</span>restaurantCapacity = capacity;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-tab-span">	</span><span class="Apple-tab-span">	</span>isRestaurantOpen = open;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>} <span class="Apple-converted-space"> </span></span></p>
-<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span></span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>public void confirmReservation() {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>if (restaurantCapacity &gt;= guestCount &amp;&amp; isRestaurantOpen) {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>System.out.println("Reservation confirmed");</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>isConfirmed = true;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>} else {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>System.out.println("Reservation denied");</span></p>
-<p class="p1"><span class="s1"><span class="Apple-tab-span">	</span><span class="Apple-tab-span">	</span><span class="Apple-tab-span">	</span>isConfirmed = false;</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>}</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>}</span></p>
-<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span></span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>public void informUser() {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>// Write conditional here</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>if (!isConfirmed) {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>System.out.println("Unable to confirm reservation, please contact restaurant.");</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>}</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>else {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">      </span>System.out.println("Please enjoy your meal!");</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>}</span></p>
-<p class="p1"><span class="s1"><br>
-</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>}</span></p>
-<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span></span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>public static void main(String[] args) {</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>Reservation partyOfThree = new Reservation(3, 12, true);</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>Reservation partyOfFour = new Reservation(4, 3, true);</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>partyOfThree.confirmReservation();</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>partyOfThree.informUser();</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>partyOfFour.confirmReservation();</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>partyOfFour.informUser();</span></p>
-<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>}</span></p>
-<p class="p1"><span class="s1">}</span></p>
-</body>
-</html>
+
+public class Reservation {
+
+      int guestCount ;
+      int restaurantCapacity ;
+      boolean isRestaurantOpen ;
+      boolean isConfirmed ;
+
+    public Reservation(int count, int capacity, boolean open) {
+        if (count & lt;1 || count & gt;8
+        ) {
+     
+            System.out.println("Invalid reservation!");
+               
+        }
+        guestCount = count;
+        restaurantCapacity = capacity;
+        isRestaurantOpen = open;
+         
+    }
+
+    public void confirmReservation() {
+        if (restaurantCapacity & gt;
+         = guestCount & amp; & amp;isRestaurantOpen
+        ) {
+     
+            System.out.println("Reservation confirmed");
+                 
+            isConfirmed = true;
+               
+        }else {
+                         
+                    System.out.println("Reservation denied");
+                    isConfirmed = false;
+    }
+         
+    }
+
+    public void informUser() {
+            // Write conditional here
+   
+        if (!isConfirmed) {
+                 
+            System.out.println("Unable to confirm reservation, please contact restaurant.");
+               
+        }
+           else {
+                         
+                    System.out.println("Please enjoy your meal!");
+    }
+         
+    }
+
+       public static void main(String[] args) {
+           
+        Reservation partyOfThree = new Reservation(3, 12, true);
+           
+        Reservation partyOfFour = new Reservation(4, 3, true);
+           
+        partyOfThree.confirmReservation();
+           
+        partyOfThree.informUser();
+           
+        partyOfFour.confirmReservation();
+           
+        partyOfFour.informUser();
+         
+    }
+}
