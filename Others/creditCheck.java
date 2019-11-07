4a8990ec589ba0a36c811ed31a083e2dce2af8d0
thestriver <wannabeamumin@gmail.com>
2019-11-07 23:06:12 +0000

    testAutomationJavaUni

--- Others/creditCheck.java
+++ Others/creditCheck.java
@@ -1,5 +1,5 @@
+package chapterFive;
 import.java.util.Scanner;
-
 
 public class InstantCreditCheck {
 
@@ -8,43 +8,45 @@ public class InstantCreditCheck {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String args[]){
-    
-    getSalary();
-    getCreditScore();
-    isUserQualified();
-    scan.close();
+    	double salary = getSalary();
+    	int creditScore = getCreditScore();
+    	boolean qualified = isUserQualified(creditScore, salary);
+    	notifyUser(qualified);
+    	scan.close();
 
 	}
-	
+	//gets salary from scannner
 	public static double getSalary(){
 		System.out.println("Enter your salary:");
     	double salary = scan.nextDouble();
     	return salary;
     
 	}
-
+	//this gets salary from scannner
 	public static int getCreditScore(){
         System.out.println("Enter your credit score:");
         int creditScore = scan.nextInt();
         return creditScore;
     }
 
-}
-
-
-	public static boolean isUserQualified(){
-	if (creditScore >= requiredCreditScore && Salary >=requiredSalary) {
+	//checks if user is qualified then return either a positive or negative boolean
+	public static boolean isUserQualified(int creditScore, double salary){
+	if (creditScore >= requiredCreditScore && salary >=requiredSalary) {
 		return true;
 		
 	}
 	else {
+		return false;
 
-	}
-
-
-}
-
-
+	}	}
+	//notifies the user on the status of his credit application
+	public static void notifyUser(boolean isQualified){
+    if(isQualified){
+        System.out.println("Congrats! You've been approved.");
+    }
+    else{
+        System.out.println("Sorry. You've been declined");
+    }	}
 
 
 }
