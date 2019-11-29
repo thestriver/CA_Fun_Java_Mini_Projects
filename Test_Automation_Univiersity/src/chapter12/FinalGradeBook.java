8982f3ac78f030c048fa4bf4aae6b22fb1a02198
infovore <wannabeamumin@gmail.com>
2019-11-29 13:19:57 +0000

    TAU-Collections

--- Test_Automation_Univiersity/src/chapter12/FinalGradeBook.java
+++ Test_Automation_Univiersity/src/chapter12/FinalGradeBook.java
@@ -1,5 +1,30 @@
 package chapter12;
 
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FinalGradeBook {
-    //call the two methods from testresults class and then make a final grade book containing each student's highest score from the two attempts
+    //call the two methods from test results class and then make a final grade book containing each student's highest score from the two attempts
+    public static void main(String[] args) {
+        finalGradeBook();
+
+    }
+    public static void finalGradeBook(){
+        Map<String,Integer> firstGrading = TestResults.getOriginalGrades();
+        Map<String, Integer> secondGrading = TestResults.getMakeUpGrades();
+
+        for (var student : secondGrading.entrySet()) {
+            Integer firstGrade = firstGrading.get(student.getKey());  //getValue?
+            Integer secondGrade = secondGrading.get(student.getKey());
+
+            if (secondGrade > firstGrade) {
+                firstGrading.put(student.getKey(),secondGrade);
+            }
+
+        }
+
+
+    }
 }
